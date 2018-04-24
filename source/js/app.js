d91c2778cae967dc5ede7a22edd1ea86f28f27b2
Damy Boerboom <damycmd@gmail.com>
2018-04-24 12:39:36 +0200

    Footer

--- source/js/app.js
+++ source/js/app.js
@@ -39,3 +39,13 @@ $(document).ready(function() {
   })
 
 })
+
+  // Including external html
+
+  $(function(){
+    var includes = $('[data-include]');
+    jQuery.each(includes, function(){
+      var file = 'blocks/' + $(this).data('include') + '.html';
+      $(this).load(file);
+    });
+  });
